b01667723dd30b36c805ce6bbe3dd8ba8f4626e0
suyog301103 <117434229+suyog301103@users.noreply.github.com>
2024-03-05 13:41:39 +0530

    added godaddy functionality

--- Recursive_model/Auth_GoDaddy.py
+++ Recursive_model/Auth_GoDaddy.py
@@ -20,14 +20,15 @@ while True :
     com_TLD_message, com_TLD_address = goDaddy_server_socket.recvfrom(16384)
     message = com_TLD_message.decode()
     message = json.loads(message)
+    print(message)
     
-    if ("cats4you" in com_TLD_message.decode().split('.')):
+    if ("cats4you" in message["Questions"]["Name"]):
         port= Services_IP["cats4you"]
     
-    elif ("darshini4you" in com_TLD_message.decode().split('.')) :
+    elif ("darshini4you" in message["Questions"]["Name"]) :
         port= Services_IP["darshini4you"]
     
-    elif ("cafe4you" in com_TLD_message.decode().split('.')) :
+    elif ("cafe4you" in message["Questions"]["Name"]) :
         port= Services_IP["cafe4you"]
 
     else :
@@ -41,7 +42,7 @@ while True :
         response["Type"] = message["Questions"]["Type"]
         response["Class"] = message["Questions"]["Class"]
         response["Address"] = port
-        goDaddy_server_socket.sendto((json.loads(response)).encode(), com_TLD_address)
+        goDaddy_server_socket.sendto((json.dumps(response)).encode(), com_TLD_address)
         continue
     
     else :
